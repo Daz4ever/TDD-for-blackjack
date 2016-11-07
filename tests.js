e2fc715d055db3dfaa27a39464af174ea7e28c09
Dominic Zenon <dominiczenon@gmail.com>
2016-11-06 22:38:20 -0500

    added

--- tests.js
+++ tests.js
@@ -57,7 +57,14 @@ describe('calculatePoints', function () {
     hand.addCard(card1);
     hand.addCard(card2);
     expect(hand.calculatePoints()).toEqual(21);
-    // expect(calculatePoints([
+    var hand2 = new Hand();
+    var card3 = new Card(2, 'diamonds');
+    var card4 = new Card(10, 'diamonds');
+    var card5 = new Card(1, 'diamonds');
+    hand.addCard(card1);
+    hand.addCard(card2);
+    hand.addCard(card3);
+    expect(hand.calculatePoints()).toEqual(13);
     //   { point: 2, suit: 'diamonds' },
     //   { point: 10, suit: 'diamonds' },
     //   { point: 1, suit: 'diamonds' }
@@ -76,15 +83,15 @@ describe('calculatePoints', function () {
 
   });
 
-});
+// });
 
-describe('newDeck', function () {
-  it('has all the cards', function () {
-    var deck = newDeck();
-    expect(deck.length).toEqual(52);
-    var stringRepresentation = deck.map(function(card) {
-      return card.point + ' of ' + card.suit;
-    }).join(',');
-    expect(stringRepresentation).toEqual('1 of spades,1 of hearts,1 of clubs,1 of diamonds,2 of spades,2 of hearts,2 of clubs,2 of diamonds,3 of spades,3 of hearts,3 of clubs,3 of diamonds,4 of spades,4 of hearts,4 of clubs,4 of diamonds,5 of spades,5 of hearts,5 of clubs,5 of diamonds,6 of spades,6 of hearts,6 of clubs,6 of diamonds,7 of spades,7 of hearts,7 of clubs,7 of diamonds,8 of spades,8 of hearts,8 of clubs,8 of diamonds,9 of spades,9 of hearts,9 of clubs,9 of diamonds,10 of spades,10 of hearts,10 of clubs,10 of diamonds,11 of spades,11 of hearts,11 of clubs,11 of diamonds,12 of spades,12 of hearts,12 of clubs,12 of diamonds,13 of spades,13 of hearts,13 of clubs,13 of diamonds');
-  });
+// describe('newDeck', function () {
+//   it('has all the cards', function () {
+//     var deck = newDeck();
+//     expect(deck.length).toEqual(52);
+//     var stringRepresentation = deck.map(function(card) {
+//       return card.point + ' of ' + card.suit;
+//     }).join(',');
+//     expect(stringRepresentation).toEqual('1 of spades,1 of hearts,1 of clubs,1 of diamonds,2 of spades,2 of hearts,2 of clubs,2 of diamonds,3 of spades,3 of hearts,3 of clubs,3 of diamonds,4 of spades,4 of hearts,4 of clubs,4 of diamonds,5 of spades,5 of hearts,5 of clubs,5 of diamonds,6 of spades,6 of hearts,6 of clubs,6 of diamonds,7 of spades,7 of hearts,7 of clubs,7 of diamonds,8 of spades,8 of hearts,8 of clubs,8 of diamonds,9 of spades,9 of hearts,9 of clubs,9 of diamonds,10 of spades,10 of hearts,10 of clubs,10 of diamonds,11 of spades,11 of hearts,11 of clubs,11 of diamonds,12 of spades,12 of hearts,12 of clubs,12 of diamonds,13 of spades,13 of hearts,13 of clubs,13 of diamonds');
+  // });
 });
